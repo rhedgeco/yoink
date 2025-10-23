c03308c0985cb73478c4b6d68a979eb266d3779a
Ryan Hedgecock <rhedgeco@gmail.com>
2025-10-22 19:56:18 -0700

    clarify errors from file read

--- src/runner/bytes.rs
+++ src/runner/bytes.rs
@@ -1,8 +1,17 @@
 use std::{fs, io, path::Path};
 
+use anyhow::anyhow;
+
 pub fn yoink(path: impl AsRef<Path>, mut write: impl io::Write) -> anyhow::Result<()> {
     let path = path.as_ref();
-    let bytes = fs::read(path)?;
+
+    // read the bytes directly from the target path
+    let bytes = fs::read(path).map_err(|err| {
+        let path_display = path.display();
+        anyhow!("'{path_display}': {err}")
+    })?;
+
+    // then write the bytes directly
     write.write(&bytes)?;
     Ok(())
 }
