b668d524bde637204c7e53093c59bd6c733fb884
Ryan Hedgecock <rhedgeco@gmail.com>
2026-07-31 21:47:23 -0700

    reformat with nixfmt

--- flake.nix
+++ flake.nix
@@ -5,14 +5,17 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    supportedSystems = ["x86_64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgsFor = nixpkgs.legacyPackages;
-  in {
-    packages = forAllSystems (system: rec {
-      yoink = pkgsFor.${system}.callPackage ./. {};
-      default = yoink;
-    });
-  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = nixpkgs.legacyPackages;
+    in
+    {
+      packages = forAllSystems (system: rec {
+        yoink = pkgsFor.${system}.callPackage ./. { };
+        default = yoink;
+      });
+    };
 }
